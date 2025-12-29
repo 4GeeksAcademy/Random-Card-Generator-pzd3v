1424b1f0ea4df0416ed364f999794ced31138002
PzD3v <ppesantez97@gmail.com>
2025-12-29 16:37:48 -0500

    funcion de generador de cartas

--- src/app.js
+++ src/app.js
@@ -1,22 +1,40 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 const suits = ["♦", "♥", "♠", "♣"]; // Diamantes, Corazones, Picas, Tréboles 🎨
-const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const values = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A",
+];
 
-function randomCard(){
-  
+function randomCard() {
+  const randomNumber = values[Math.floor(Math.random() * values.length)];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+  console.log(`Tu carta es: ${randomNumber} de ${randomSuit}`);
 }
 
-const buttonCharge = document.createElement("button")
-
-
-window.onload = function() {
+/* window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+randomCard()
+  
+}; */
 
-};
+const buttonCharge = document.getElementById("charge")
+buttonCharge.addEventListener("click",randomCard)
+
+
