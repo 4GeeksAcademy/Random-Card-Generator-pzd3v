2bd54747150776ede57448c7c25e168860aded4c
PzD3v <ppesantez97@gmail.com>
2025-12-29 16:12:03 -0500

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const suits = ["♦", "♥", "♠", "♣"]; // Diamantes, Corazones, Picas, Tréboles 🎨
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+function randomCard(){
+  
+}
+
+const buttonCharge = document.createElement("button")
+
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
 };
